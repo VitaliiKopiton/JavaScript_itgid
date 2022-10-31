76ebeda701aa581643fe771a61f5e6c5ef74a72b
Vitalii Kopiton <dd150285kvn@gmail.com>
2022-11-01 01:26:22 +0200

    unit_14

--- unit_14/unit_14.js
+++ unit_14/unit_14.js
@@ -3,7 +3,19 @@ const param = {
     "appid": "fc8d31d28c201415639891717ed52d7b"
 }
 
+const cities = {
+    "6548066": "Kyiv",
+    "706483": "Kharkiv",
+    "698740": "Odesa",
+    "709930": "Dnipro",
+    "709717": "Donetsk"
+}
+
 let out = document.querySelector('.out');
+
+// document.querySelector('#select') = 
+
+
 
 getWeather();
 
@@ -15,11 +27,17 @@ function getWeather() {
         .then(function (response) {
             return response.json();;
         })
-        // .then(showWeater);
-        .then(function (date) {
-            // console.log(date.main.temp.toFixed(1));
-            out.innerHTML = (date.main.temp - 273.15).toFixed(1);
-        })
+        .then(showWeater);
+    // .then(function (date) {
+    //     // console.log(date.main.temp.toFixed(1));
+    //     // out.innerHTML = (date.main.temp - 273.15).toFixed(1);
+    // })
+
+}
+
+function showWeater(date) {
+
+    out.innerHTML = (date.main.temp - 273.15).toFixed(1);
 
 }
 
