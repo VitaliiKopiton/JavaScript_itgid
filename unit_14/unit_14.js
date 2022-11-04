8e34a157a7678a7ea03c245a947668de7eb27e8c
Vitalii Kopiton <dd150285kvn@gmail.com>
2022-11-04 02:58:59 +0200

    unit_14_API_wind

--- unit_14/unit_14.js
+++ unit_14/unit_14.js
@@ -4,18 +4,21 @@ const param = {
 }
 
 const cities = {
-    "6548066": "Kyiv",
-    "706483": "Kharkiv",
-    "698740": "Odesa",
-    "709930": "Dnipro",
-    "709717": "Donetsk"
+    "Kyiv": "703448",
+    "Kharkiv": "706483",
+    "Odesa": "698740",
+    "Dnipro": "709930",
+    "Donetsk": "709717"
 }
 
-let out = document.querySelector('.out');
+for (key in cities) {
+    let newCity = document.createElement('option');
+    newCity.value = cities[key];
+    newCity.innerHTML = key;
+    document.querySelector('.city').append(newCity);
+}
 
-// document.querySelector('#select') = 
-
-
+// let out = document.querySelector('.out');
 
 getWeather();
 
@@ -28,18 +31,25 @@ function getWeather() {
             return response.json();;
         })
         .then(showWeater);
-    // .then(function (date) {
-    //     // console.log(date.main.temp.toFixed(1));
-    //     // out.innerHTML = (date.main.temp - 273.15).toFixed(1);
-    // })
 
 }
+
+
+let nameCity = document.querySelector('.name');
+let temp = document.querySelector('.temp');
+let icon = document.querySelector('.icon');
+let description = document.querySelector('.description');
 
 function showWeater(date) {
 
-    out.innerHTML = (date.main.temp - 273.15).toFixed(1);
+    nameCity.innerHTML = date.name;
+    temp.innerHTML = (date.main.temp - 273.15).toFixed(1) + '&#176C';
+    icon.innerHTML = `<img src="http://openweathermap.org/img/wn/${date.weather[0].icon}@2x.png">`;
+    description.innerHTML = date.weather[0].description;
+    console.log(date);
 
 }
+
 
 document.querySelector('.city').onchange = function () {
     getWeather();
